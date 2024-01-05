df06b68279dabd7b675bf9f0700610fb121ff025
hjhjhj <153933332+hjkim22@users.noreply.github.com>
2024-01-05 15:37:46 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+num1_1, num2_1 = 3, 2
+num1_2, num2_2 = 10, 5
+
+print(solution(num1_1, num2_1))
+print(solution(num1_2, num2_2))

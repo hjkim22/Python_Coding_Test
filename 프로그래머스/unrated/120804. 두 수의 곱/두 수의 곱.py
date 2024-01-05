86d9a6ed122652321e1e71d3d19a605bb6c165a8
hjhjhj <153933332+hjkim22@users.noreply.github.com>
2024-01-05 15:35:15 +0900

    [level unrated] Title: 두 수의 곱, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120804. 두 수의 곱/두 수의 곱.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
+
+num1_1, num1_2 = 3, 27
+num2_1, num2_2 = 4, 19
+
+print(solution(num1_1, num2_1))
+print(solution(num1_2, num2_2))

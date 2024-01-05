5b9a7cf5dd143ef42d37167c9dd70b0a69d0ad16
hjhjhj <153933332+hjkim22@users.noreply.github.com>
2024-01-05 15:30:58 +0900

    [level unrated] Title: 몫 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.py
@@ -0,0 +1,10 @@
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+num1_1, num1_2 = 10, 7
+num2_1, num2_2 = 5, 2
+
+
+print(solution(num1_1, num1_2))
+print(solution(num2_1, num2_2))

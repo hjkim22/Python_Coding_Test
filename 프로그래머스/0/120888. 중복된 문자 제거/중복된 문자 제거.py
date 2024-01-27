cb2025ecba95b3d27b7683f8de318da3402a938f
lllh <153933332+hjkim22@users.noreply.github.com>
2024-01-27 22:05:29 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -0,0 +1,13 @@
+def solution(my_string):
+    result = ""
+    seen = set()
+
+    for char in my_string:
+        if char not in seen:
+            result += char
+            seen.add(char)
+
+    return result
+
+print(solution("people"))
+print(solution("We are the world"))

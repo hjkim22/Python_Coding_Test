b68876f3d31c78574dc47e6edcd940108bbf99dc
lllh_jiin <153933332+hjkim22@users.noreply.github.com>
2024-02-29 13:33:29 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.py
@@ -0,0 +1,10 @@
+def solution(num_list, n):
+    return num_list[:n]
+
+num_list1 = [2, 1, 6]
+n1 = 1
+print(solution(num_list1, n1))
+
+num_list2 = [5, 2, 1, 7, 5]
+n2 = 3
+print(solution(num_list2, n2))

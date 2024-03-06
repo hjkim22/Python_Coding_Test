bfd2179e63038d874b2a07f7bfb55c122abe6413
lllh <153933332+hjkim22@users.noreply.github.com>
2024-03-07 00:10:08 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,8 @@
+def solution(todo_list, finished):
+    result = []
+    for i in range(len(todo_list)):
+        if not finished[i]:
+            result.append(todo_list[i])
+    return result
+
+print(solution(["problemsolving", "practiceguitar", "swim", "studygraph"], [True, False, True, False]))

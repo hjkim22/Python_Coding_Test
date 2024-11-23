6205ea9a6443068c7757665ca30b0eea5a67ef31
lllh <153933332+hjkim22@users.noreply.github.com>
2024-11-23 12:10:06 +0900

    [level 2] Title: 구명보트, Time: 8.84 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 8.63 ms
+메모리: 10.6 MB, 시간: 8.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 10일 10:14:33
+2024년 11월 23일 12:10:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,13 @@
 def solution(people, limit):
+    answer, left, right = 0, 0, len(people) - 1
     people.sort()
-    answer = 0
-    light = 0
-    heavy = len(people) - 1
     
-    while light <= heavy:
-        if people[light] + people[heavy] <= limit:
-            light += 1  
-        heavy -= 1 
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            right -= 1
+        else:
+            right -= 1
         answer += 1
     
     return answer
-
-print(solution([70, 50, 80, 50], 100))
-print(solution([70, 80, 50], 100))
